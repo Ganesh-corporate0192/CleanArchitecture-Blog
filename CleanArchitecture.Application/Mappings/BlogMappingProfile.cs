e43f9abe36c60add834a7ae0a803f0a86b974184
Ganesh-corporate0192 <EMAIL_USED_IN_GITHUB_REPO>
2026-02-25 12:14:44 +0530

    Mapping done

---
+++ CleanArchitecture.Application/Mappings/BlogMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Application.Features.Blogs.Commands.CreateBlog;
+using CleanArchitecture.Application.DTOs;
+
+namespace CleanArchitecture.Application.Mappings;
+
+public class BlogMappingProfile : Profile
+{
+    public BlogMappingProfile()
+    {
+        CreateMap<CreateBlogCommand, Blog>();
+
+        CreateMap<Blog, BlogResponseDto>();
+    }
+}
