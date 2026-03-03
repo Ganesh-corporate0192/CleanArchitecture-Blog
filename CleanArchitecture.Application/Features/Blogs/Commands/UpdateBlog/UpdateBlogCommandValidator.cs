f0abb4e239e64d9102257de89443a3f250142389
Ganesh-corporate0192 <EMAIL_USED_IN_GITHUB_REPO>
2026-03-03 10:55:14 +0530

    Update command Validtion

---
+++ CleanArchitecture.Application/Features/Blogs/Commands/UpdateBlog/UpdateBlogCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace CleanArchitecture.Application.Features.Blogs.Commands.UpdateBlog;
+
+public class UpdateBlogCommandValidator
+    : AbstractValidator<UpdateBlogCommand>
+{
+    public UpdateBlogCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Blog Id is required");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .MaximumLength(100);
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required");
+
+        RuleFor(x => x.Author)
+            .NotEmpty().WithMessage("Author is required");
+
+        RuleFor(x => x.ImageUrl)
+            .NotEmpty().WithMessage("ImageUrl is required")
+            .Must(uri => Uri.IsWellFormedUriString(uri, UriKind.Absolute))
+            .WithMessage("Invalid Image URL");
+    }
+}
